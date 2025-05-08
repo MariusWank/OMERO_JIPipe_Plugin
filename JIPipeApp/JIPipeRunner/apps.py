f1b7abaf4827bb77f67a9be66f3023544153e583
MariusWank <mariuswank@web.de>
2025-05-08 10:12:46 +0200

    Deleted uneccesary copy

--- JIPipeApp/JIPipeRunner/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class JipiperunnerConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "JIPipeRunner"
--- JIPipeApp/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name='JIPipeRunner',
-    version='0.0.1',
-    description="OMERO plugin to run JIPipe",
-    packages=find_packages(),
-    keywords=['omero', 'jipipe'],
-)
