45d62143a5ea84bf0783d6fe7d8895361ce7bfd3
MariusWank <mariuswank@web.de>
2025-05-28 16:43:06 +0200

    Added new URL for listing .jip files on the server and renamed other links to better represent what they are doing

--- JIPipeRunner/urls.py
+++ JIPipeRunner/urls.py
@@ -2,10 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('jipipe_runner_index/<int:project_id>', views.jipipe_runner_index, name='jipipe_runner_index'),
-    path('get_jipipe_config/<int:project_id>/json/', views.get_jipipe_config, name='get_jipipe_config'),
+    path('jipipe_runner_index/', views.jipipe_runner_index, name='jipipe_runner_index'),
+    path('get_jipipe_config/<int:jip_file_id>/', views.get_jipipe_config, name='get_jipipe_config'),
     path("jipipe_start_job/", views.start_jipipe_job, name="jipipe_start_job"),
-    path("fetch_jipipe_logs/<str:job_id>/", views.fetch_jipipe_logs, name="fetch_jipipe_logs"),
+    path("fetch_jipipe_logs/<str:job_uuid>/", views.fetch_jipipe_logs, name="fetch_jipipe_logs"),
     path("stop_jipipe_job/", views.stop_jipipe_job, name="stop_jipipe_job"),
     path("list_jipipe_jobs/", views.list_jipipe_jobs, name="list_jipipe_jobs"),
+    path("list_jipipe_files/", views.list_jipipe_files, name="list_jipipe_files"),
 ]
