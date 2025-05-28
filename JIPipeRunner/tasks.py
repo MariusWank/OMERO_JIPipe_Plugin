323d7adc321b36f901d741b9da79b2542ab2fac0
MariusWank <mariuswank@web.de>
2025-05-28 16:42:04 +0200

    Added proper documentation in the form of comments and docstring

--- JIPipeRunner/tasks.py
+++ JIPipeRunner/tasks.py
@@ -4,26 +4,46 @@ from pathlib import Path
 from django.core.cache import cache
 from omero.config import ConfigXml
 import signal
+from django.conf import settings
 
-# Turn SIGTERM into KeyboardInterrupt so you can catch it
+# Turn SIGTERM into KeyboardInterrupt so it can be caught by the task (necessary to shutdown child processes)
 signal.signal(signal.SIGTERM, lambda signum, frame: (_ for _ in ()).throw(KeyboardInterrupt()))
 
+"""
+This task runs a JIPipe project in the background using ImageJ CLI.
+It creates temporary directories for input and output, runs the JIPipe 
+project using xvfb-run to handle GUI elements, and logs the output to a 
+specified log file. When the task is interrupted or fails, it cleans up 
+the temporary directories and logs the error.
+
+param jipipe_project_config: JSON configuration of the JIPipe project
+param job_uuid: Unique identifier for the JIPipe job
+param omero_user_name: Username of the OMERO user running the job
+param jipipe_log_file_path: Path to the log file for the JIPipe job
+"""
 @shared_task(bind=True)
-def run_jipipe_task(self, project_config, job_uuid, user_name, log_file_path):
+def run_jipipe_task(self, jipipe_project_config, job_uuid, omero_user_name, jipipe_log_file_path):
+
+    # Initialize logging
     log = logging.getLogger(__name__)
 
+    # Create temporary directories for handling input and output
     temp_input = tempfile.mkdtemp()
     temp_output = tempfile.mkdtemp()
 
     try:
+        # Save the JIPipe project configuration to a file to access it via ImageJ CLI
         jip_project_file = Path(temp_input) / 'JIPipeProject.jip'
         with open(jip_project_file, 'w') as f:
-            json.dump(project_config, f)
+            json.dump(jipipe_project_config, f)
 
+        # Get the ImageJ path from the OMERO configuration to run JIPipe on
         cfg_file = os.path.join(os.environ["OMERODIR"], "etc", "grid", "config.xml")
         cfg = ConfigXml(cfg_file, read_only=True)
         imagej_path = cfg.as_map().get("omero.web.imagej")
 
+        # Define the command to run JIPipe using ImageJ CLI
+        # TODO: Make memory configurable
         command = [
             'xvfb-run', '-a', imagej_path,
             '-Dorg.apache.logging.log4j.simplelog.StatusLogger.level=ERROR',
@@ -34,9 +54,10 @@ def run_jipipe_task(self, project_config, job_uuid, user_name, log_file_path):
             'run', '--project', str(jip_project_file),
             '--output-folder', temp_output,
         ]
-
-        with open(log_file_path, 'w') as log_file:
+        # Run the command and log the output
+        with open(jipipe_log_file_path, 'w') as log_file:
             log_file.write("Executable ImageJ at: " + imagej_path + "\n")
+
             process = subprocess.Popen(
                 command,
                 stdout=subprocess.PIPE,
@@ -46,35 +67,41 @@ def run_jipipe_task(self, project_config, job_uuid, user_name, log_file_path):
                 preexec_fn=os.setsid,
             )
 
-            cache.set(job_uuid, process.pid, timeout=None)
-
+            # Write the output of the process to the log file
             for line in process.stdout:
                 log_file.write(line)
                 log_file.flush()
 
+            # Wait for the process to complete
             process.wait()
+
             log_file.write(f"\n[ JIPipe exited with code {process.returncode} ]\n")
+
     except KeyboardInterrupt:
+        # On receiving SIGTERM, close cfg and clean up cache and temporary directories before exiting
         cfg.close()
-        cache.delete(job_uuid)
-        user_key = f"active_jipipe_jobs_{user_name}"
+        user_key = f"active_jipipe_jobs_{omero_user_name}"
         active = set(cache.get(user_key, []))
         active.discard(job_uuid)
         cache.set(user_key, active, timeout=None)
         shutil.rmtree(temp_input)
         shutil.rmtree(temp_output)
         os.killpg(process.pid, signal.SIGTERM)
+        
     except Exception as e:
-        with open(log_file_path, 'a') as log_file:
+        # Log any exceptions that occur during the task and throw an exception in OMERO log
+        with open(jipipe_log_file_path, 'a') as log_file:
             log_file.write(f"\nERROR in JIPipe background job: {e}\n")
         log.exception("Error in Celery JIPipe task")
 
     finally:
+        # Close cfg and clean up cache and temporary directories
         cfg.close()
-        cache.delete(job_uuid)
-        user_key = f"active_jipipe_jobs_{user_name}"
+        user_key = f"active_jipipe_jobs_{omero_user_name}"
         active = set(cache.get(user_key, []))
+        log.info(f"Previous active JIPipe jobs for user {omero_user_name}: {active}")
         active.discard(job_uuid)
         cache.set(user_key, active, timeout=None)
+        log.info(f"Updated active JIPipe jobs for user {omero_user_name}: {cache.get(user_key, [])}")
         shutil.rmtree(temp_input)
         shutil.rmtree(temp_output)
